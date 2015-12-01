8ec9bb96447b4ddebd24218b2215554a28182119
Nikita Kasyanov <nik.kasyanov@gmail.com>
2015-12-01 22:57:13 +0300

    index page

---
+++ src/main/webapp/scripts/locale.js
@@ -0,0 +1,182 @@
+/**
+ * Created by kascode on 02.09.15.
+ */
+var locale = {
+  "ru-RU": {
+    "lang_name": "Русский",
+    "alt_lang": "English",
+    "alt_lang_short": "en-US",
+    "title": "Smooth — движение без барьеров",
+    "hero": {
+      "title": "SMOOTH — первый сервис построения маршрутов по городу с обходом препятствий",
+      "text": "Мы все имеем различные физические возможности, но наш город должен быть доступным для каждого. SMOOTH " +
+      "позволяет путешествовать по городу людям разной степени мобильности. Каждый человек может добавить информацию о " +
+      "препятствии на его пути. В дальнейшем это обязательно поможет кому-то безопасно достичь своей цели."
+    },
+    "about": "описание",
+    "features": "возможности",
+    "team": "команда",
+    "contacts": "контакты",
+    "open_app": "Открыть приложение",
+    "find_more": "Узнать больше",
+    "video_link": "Видео презентация",
+    "about_text": "Сервис Smooth доступен на всех современных смартфонах в виде вэб-приложения. Вам достаточно лишь " +
+    "открыть ссылку в браузере и построить маршрут либо добавив информацию о препятствии.",
+    "pavement_cond": "Состояние тротураов",
+    "pavement_cond_text": "Благодаря открытым данным и нашим пользователям Smooth знает о проблемах на пешеходных " +
+    "путях и находит доступный маршрут.",
+    "ramp_steps": "Уклоны, ступеньки, поребрики",
+    "ramp_steps_text": "Отсутствует спуск на пешеходном переходе, ступеньки на тротуаре, слишком крутой уклон. " +
+    "Smooth избавит от риска оказаться в тупике.",
+    "transport_acc": "Оборудованность транспорта",
+    "transport_acc_text": "Smooth найдёт ближайший транспорт с низким полом. При прокладке маршрута Smooth предлагает " +
+    "только доступный транспорт.",
+    "bumaga": "Газета \"Бумага\"",
+    "bumaga_text": "Статья о запуске бета-версии сервиса SMOOTH",
+    "ihuman": "Социальный портал \"Я Человек\"",
+    "ihuman_text": "Публикация о запуске нашего cервиса с кратким обзором проблем доступности",
+    "ntv": "НТВ-Петербург",
+    "ntv_text": "Репортаж о Петербургском Open Data Хакатоне 2014 и его победителе — проекте Smooth",
+    "delovoy": "Деловой Петербург",
+    "delovoy_text": "Статья в Деловом Петербурге о проектах, основанных на открытых данных",
+    "milestones": "Этапы развития проекта",
+    "milestone_1": "Ноябрь 2014 — запуск альфа-версии Smooth после победы на хакатоне Hack4Piter 2014",
+    "milestone_2": "Декабрь 2014 — улучшен алгоритм построения маршрутов. Поддержка маршрутизации на больших областях",
+    "milestone_3": "Весна 2015 — Полевые исследования и тестирование. Анализ различных способов объедениения данных из " +
+    "разных источников. Разработка системы обхода препятствий",
+    "milestone_4": "Июнь 2015 — добавление возможности загрузки препятствий с фотографией. Типизация препятствий. " +
+    "Интеграция общественного транспорта",
+    "milestone_5": "Июль 2015 - Множество мелких улучшений - отрисовка маршрута, обновление интерфейса для десктопа и в " +
+    "мобильной версии",
+    "milestone_6": "Август 2015 - Англоязычная версия сервиса. Отрисовка тротуаров в OSM. До сих пор является главной " +
+    "задачей",
+    "in_work": "В работе",
+    "ap1": "Мобильная версия",
+    "ap2": "Инструмент для отслеживания изменений в тротуарной сети OpenStreetMap",
+    "ap3": "Тестирование алгоритма обхода препятствий",
+    "ap4": "Запуск зоны в центре Петербурга с полным функционалом",
+    "ap5": "Поиск гранта на развитие проекта. Мы подошли к задачам требующим не столько сложной, сколько объёмной работы",
+    "ui": {
+      "goto": "Перейти"
+    }
+  },
+  "en-US": {
+    "lang_name": "English",
+    "alt_lang": "Русский",
+    "alt_lang_short": "ru-RU",
+    "title": "Smooth — move without obstacles",
+    "hero": {
+      "title": "SMOOTH is world’s first interactive navigation service for pedestrians with limited mobility. It works " +
+      "just like a standard route planner, but it generates routes, taking into account and avoiding potential " +
+      "obstacles.",
+      "text": "We all have different levels of physical abilities and our city has to be accessible for all. SMOOTH " +
+      "allows people with different levels of mobility to move around the city easily. It is a living service and we " +
+      "encourage our users to add data about new obstacles they encounter."
+    },
+    "about": "About",
+    "features": "Features",
+    "team": "Team",
+    "contacts": "Contacts",
+    "open_app": "Open the app",
+    "find_more": "Find out more",
+    "video_link": "Video: What is SMOOTH?",
+    "about_text": "SMOOTH is a web application which works on all types of smartphones. Just open the link and either " +
+    "build your route or add information about an obstacle.",
+    "pavement_cond": "Pavement conditions",
+    "pavement_cond_text": "SMOOTH uses both open-source and user-generated data in order to get the most up-to-date " +
+    "and relevant information about the sidewalks, the condition they are in, and any obstacles on the way. All of " +
+    "this information is incorporated into the route-planning functionality of SMOOTH.",
+    "ramp_steps": "Ramps, steps, sides of the road",
+    "ramp_steps_text": "Whether it’s a missing ramp on the underpass, steps on the pavement, or a steep road, with " +
+    "SMOOTH you won’t get stuck!.",
+    "transport_acc": "Public transport accessibility",
+    "transport_acc_text": "SMOOTH will find the nearest bus route served by low-floor buses. When calculating a route, " +
+    "SMOOTH always chooses only accessible forms of public transport.",
+    "bumaga": "\"Bumaga\" newspaper",
+    "bumaga_text": "Read an article about the launch of the SMOOTH beta",
+    "ihuman": "Social portal \"I'm a Person\"",
+    "ihuman_text": "Read an article about the launch of the SMOOTH service (includes a short review of city " +
+    "accessibility issues)",
+    "ntv": "NTV-Petersburg",
+    "ntv_text": "Video report from the Spb Open Data Hackathon 2014, featuring the winner – SMOOTH",
+    "delovoy": "Delovoy Peterburg",
+    "delovoy_text": "Read an article about projects based on open-source data",
+    "milestones": "Project development milestones",
+    "milestone_1": "November 2014 – The SMOOTH alpha version was launched after winning the Hack4Piter2014 Hackathon",
+    "milestone_2": "December 2014 – The routing algorithm was improved. Routing in large areas was launched",
+    "milestone_3": "Spring 2015 - Testing in the field. Different ways of open data aggregation were analyzed. An " +
+    "algorithm featuring obstacle avoidance was developed",
+    "milestone_4": "June 2015 - Photo upload functionality added. Obstacles types added. Public transport " +
+    "integration algorithm added",
+    "milestone_5": "July 2015 - Many minor improvements: route drawing, desktop and mobile interface updates",
+    "milestone_6": "September 2015 - English version of SMOOTH. Improving coverage of pedestrian routes in OSM format " +
+    "still a main task",
+    "in_work": "Current action points",
+    "ap1": "Mobile version bug fixes",
+    "ap2": "New tool which allows us to track all changes to sidewalk maps in OpenStreetMap",
+    "ap3": "Testing of obstacle avoidance algorithm",
+    "ap4": "Launch of zones in SPB city centre featuring full functionality",
+    "ap5": "We need more hands, not for much coding but for improving OSM and testing",
+    "ui": {
+      "goto": "Open"
+    }
+  }
+};
+
+/**
+ * Compiles array of languages
+ * each language represented by object with language short name and full name
+ * returns Array of Objects {name, abbr}
+ */
+locale.getLanguages = function () {
+  var langs = [];
+
+  for (var l in locale) {
+    if (locale.hasOwnProperty(l) && l.indexOf('-') > -1 && typeof locale[l] === 'object')
+      langs.push({
+        name: locale[l]["lang_name"],
+        abbr: l
+      });
+  }
+
+  return langs;
+};
+
+locale.setLanguage = function (lang) {
+  var t;
+  if (typeof this[lang] !== 'undefined') {
+    if (localStorage.getItem('smooth') === null) {
+      localStorage.setItem('smooth', {});
+    }
+    t = localStorage.getItem('smooth');
+    t.lang = lang;
+    localStorage.setItem('smooth', t);
+    this.currentLang = 'ru-RU';
+  }
+};
+
+locale.currentLang = 'ru-RU';
+
+locale.localize = function (phrase) {
+  var props = [this.currentLang];
+  var res;
+
+  console.log("Localize phrase:", phrase);
+
+  if (phrase.indexOf('.') !== -1) {
+    props = props.concat(phrase.split('.'));
+  } else {
+    props.push(phrase);
+  }
+
+  res = this[props[0]];
+
+  console.log(res, props);
+
+  for (var i = 1; i < props.length; i++) {
+    res = res[props[i]];
+    console.log("res", res);
+  }
+
+  return res;
+};
---
+++ src/main/webapp/scripts/scripts.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {
+
+	/***************** Waypoints ******************/
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInLeft');
+	}, {
+		offset: '75%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated bounceInDown');
+	}, {
+		offset: '75%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+
+	/***************** Flickity ******************/
+
+	$('#featuresSlider').flickity({
+		cellAlign: 'left',
+		contain: true,
+		prevNextButtons: false
+	});
+
+	$('#showcaseSlider').flickity({
+		cellAlign: 'left',
+		contain: true,
+		prevNextButtons: false,
+		imagesLoaded: true
+	});
+
+	/***************** Fancybox ******************/
+
+	$(".youtube-media").on("click", function(e) {
+		var jWindow = $(window).width();
+		if (jWindow <= 768) {
+			return;
+		}
+		$.fancybox({
+			href: this.href,
+			padding: 4,
+			type: "iframe",
+			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+		});
+		return false;
+	});
+
+});
+
+$(document).ready(function() {
+	$("a.single_image").fancybox({
+		padding: 4,
+	});
+});
+
+/***************** Nav Transformicon ******************/
+
+/* When user clicks the Icon */
+$(".nav-toggle").click(function() {
+	$(this).toggleClass("active");
+	$(".overlay-boxify").toggleClass("open");
+});
+
+/* When user clicks a link */
+$(".overlay ul li a").click(function() {
+	$(".nav-toggle").toggleClass("active");
+	$(".overlay-boxify").toggleClass("open");
+});
+
+/* When user clicks outside */
+$(".overlay").click(function() {
+	closeNavOverlay();
+});
+
+function closeNavOverlay() {
+	$(".nav-toggle").toggleClass("active");
+	$(".overlay-boxify").toggleClass("open");
+}
+
+/***************** Language Switch *******************/
+
+$('.js-lang').click(function (e) {
+	console.log("lang");
+  var $t = $(this);
+	var l = $t.data('lang');
+
+  locale.setLanguage(l);
+
+  $t.attr('data-lang', locale.localize('alt_lang_short'));
+  $t.html(locale.localize('lang_name'));
+
+  $('*:not([data-phrase=""])').each(function () {
+    $(this).html(locale.localize($(this).data('phrase')));
+  });
+
+	e.preventDefault();
+});
+
+/***************** Smooth Scrolling ******************/
+
+$('a[href*=#]:not([href=#])').click(function() {
+	if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		if (target.length) {
+			$('html,body').animate({
+				scrollTop: target.offset().top
+			}, 2000);
+			return false;
+		}
+	}
+});
